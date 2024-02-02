767d2ae28805006cb65df2bce3ad20dcd4608f77
Pina, Joao <joao.pina@sky.uk>
2024-02-02 09:55:17 +0000

    first commit

---
+++ src/main/java/users/api/hexagonal/users/api/hexagonal/UsersApiHexagonalApplication.java
@@ -0,0 +1,13 @@
+package users.api.hexagonal.users.api.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApiHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/users/api/hexagonal/users/api/hexagonal/UsersApiHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package users.api.hexagonal.users.api.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
